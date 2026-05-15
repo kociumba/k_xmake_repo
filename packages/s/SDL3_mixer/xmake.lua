267b020dfe53c0f29842b8cdb24da3a21ce50c4e
kociumba <79542688+kociumba@users.noreply.github.com>
2026-05-15 14:10:53 +0200

    add sdl3 injection to the mixer lib

--- packages/s/SDL3_mixer/xmake.lua
+++ packages/s/SDL3_mixer/xmake.lua
@@ -7,9 +7,15 @@ package("SDL3_mixer")
 
     set_urls("https://github.com/libsdl-org/SDL_mixer.git")
 
+    on_load(function (package)
+        package:add("deps", "libsdl3", { configs = { shared = package:config("shared") }})
+    end)
+
     on_install(function (package)
-        local configs = {"-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF") }
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        import("package.tools.cmake").install(package, config)
+        local configs = {"-DSDL3MIXER_VENDORED=OFF", "-DSDL3MIXER_SAMPLES=OFF"}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+
+        import("package.tools.cmake").install(package, configs)
     end)
 package_end()
