871495c26d5c255af774d44ad465e3e42b700add
kociumba <79542688+kociumba@users.noreply.github.com>
2026-05-27 22:16:08 +0200

    fix xkbcommon dependency on linux for libuiohook

--- packages/l/libuiohook/xmake.lua
+++ packages/l/libuiohook/xmake.lua
@@ -9,7 +9,7 @@ package("libuiohook")
         on_load( function (package)
             package:add("deps", "libx11", { configs = { shared = package:config("shared") } })
             package:add("deps", "libxtst", { configs = { shared = package:config("shared") } })
-            package:add("deps", "libxkbcommon", { configs = { shared = package:config("shared") } })
+            package:add("deps", "libxkbcommon", { configs = { x11 = true, shared = package:config("shared") } })
         end)
     end
 
